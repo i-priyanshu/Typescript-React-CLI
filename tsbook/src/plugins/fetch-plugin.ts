b262267c11e81166081d7a6e12cb92ee84d5d364
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-04-21 01:49:42 +0530

    Added CSS file support in import statements

--- tsbook/src/plugins/fetch-plugin.ts
+++ tsbook/src/plugins/fetch-plugin.ts
@@ -11,7 +11,7 @@ export const fetchPlugin = (inputCode: string) => {
     name: "fetch-plugin",
     setup(build: esbuild.PluginBuild) {
       build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log("onLoad", args);
+        // console.log("onLoad", args);
         if (args.path === "index.js") {
           return {
             loader: "jsx",
@@ -25,15 +25,30 @@ export const fetchPlugin = (inputCode: string) => {
         );
 
         //if it is , return it immediately
-        if (cachedResult) {
-          return cachedResult;
-        }
+        // if (cachedResult) {
+        //   return cachedResult;
+        // }
 
         const { data, request } = await axios.get(args.path);
 
+        const fileType = args.path.match(/.css$/) ? "css" : "jsx";
+
+        const escaped = data
+          .replace(/\n/g, "")
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'");
+
+        const contents =
+          fileType === "css"
+            ? `
+            const style = document.createElement('style');
+            style.innerText = '${escaped}';
+            document.head.appendChild(style);    
+        `
+            : data;
         const result: esbuild.OnLoadResult = {
           loader: "jsx",
-          contents: data,
+          contents,
           resolveDir: new URL("./", request.responseURL).pathname,
         };
         // store response in cache
