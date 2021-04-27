619145f301775c1e9b75ce57b843ee38b0571a8e
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-04-27 13:19:36 +0530

    :bug: resetting the Iframe cotents

--- tsbook/src/index.tsx
+++ tsbook/src/index.tsx
@@ -6,6 +6,7 @@ import { fetchPlugin } from "./plugins/fetch-plugin";
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
 
@@ -24,6 +25,8 @@ const App = () => {
     if (!ref.current) {
       return;
     }
+    iframe.current.srcdoc = html;
+
     const result = await ref.current.build({
       entryPoints: ["index.js"],
       bundle: true,
@@ -35,12 +38,22 @@ const App = () => {
       },
     });
     // console.log(result);
-
-    setCode(result.outputFiles[0].text);
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
   };
 
   const html = `
-  <script>${code}</script>
+<html>
+   <head></head>
+  <body>
+   <div id="root"></div>
+   <script>
+      window.addEventListener('message' , (event) => {
+       eval(event.data);
+       },false);
+    </script>
+  </body>
+</html>
 `;
 
   return (
@@ -53,7 +66,7 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
-      <iframe sandbox="" srcDoc={html} />
+      <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html} />
     </div>
   );
 };
