4cdb9d9def0d7aaf36b4536888c0f3fda82b099c
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-04-28 14:56:35 +0530

    :art: Added Styling

--- tsbook/src/components/code-editor.tsx
+++ tsbook/src/components/code-editor.tsx
@@ -1,3 +1,4 @@
+import './code-editor.css';
 import {useRef} from 'react';
 import MonacoEditor, { EditorDidMount } from "@monaco-editor/react";
 import prettier from 'prettier';
@@ -30,14 +31,15 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
          useTabs: false,
          semi: true,
          singleQuote: true
-      })
+
+      }).replace(/\n$/ , '');
       //set the formatted value back in the editor
       editorRef.current.setValue(formatted);
   };
 
   return (
-      <div>
-        <button onClick={onFormat}>Format</button>
+    <div className="editor-wrapper">
+        <button className="button button-format is-primary is-small"  onClick={onFormat}>Format</button>
     <MonacoEditor
       editorDidMount={onEditorDidMount}
       value={initialValue}
