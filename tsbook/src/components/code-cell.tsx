efcc4df4ec4b96cf96db70aaf0bed9cbbda2798e
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-05-01 11:47:14 +0530

    Added Resizable Bar

--- tsbook/src/components/code-cell.tsx
+++ tsbook/src/components/code-cell.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import CodeEditor from "./code-editor";
-import Preview from './preview';
-import bundle from '../bundler';
+import Preview from "./preview";
+import bundle from "../bundler";
+import Resizable from "./resizable";
 
 const CodeCell = () => {
-  const [code , setCode] = useState('');
+  const [code, setCode] = useState("");
   const [input, setInput] = useState("");
 
   const onClick = async () => {
@@ -12,20 +13,14 @@ const CodeCell = () => {
     setCode(output);
   };
 
-
   return (
-    <div>
-      <CodeEditor
-        initialValue=""
-        onChange={(value) => setInput(value)}
-      />
-      <textarea
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      ></textarea>
-      <div><button onClick={onClick}>Submit</button></div>
-      <Preview code={code} />
-    </div>
+    <Resizable direction="vertical">
+      <div style={{ height: "100%", display: "flex", flexDirection: "row" }}>
+        <CodeEditor initialValue="" onChange={(value) => setInput(value)} />
+
+        <Preview code={code} />
+      </div>
+    </Resizable>
   );
 };
 
