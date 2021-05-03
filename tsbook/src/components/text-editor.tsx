ce016dbdb4f11b3058855345e6a727f0939f4ed9
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-05-03 20:52:36 +0530

    :art: improved Styling and UI

--- tsbook/src/components/text-editor.tsx
+++ tsbook/src/components/text-editor.tsx
@@ -1,11 +1,24 @@
-import { useState, useEffect } from "react";
+import "./text-editor.css";
+import { useState, useEffect, useRef } from "react";
 import MDEditor from "@uiw/react-md-editor";
 
 const TextEditor: React.FC = () => {
+  const ref = useRef<HTMLDivElement | null>(null);
   const [editor, setEditor] = useState(false);
+  const [value, setValue] = useState("# Header");
 
   useEffect(() => {
-    const listener = () => {
+    const listener = (event: MouseEvent) => {
+      if (
+        ref.current &&
+        event.target &&
+        ref.current.contains(event.target as Node)
+      ) {
+        console.log("elment clicked on is inside editor...");
+        return;
+      }
+      console.log("element clicked is not inside Editor");
+
       setEditor(false);
     };
     document.addEventListener("click", listener, { capture: true });
@@ -17,15 +30,17 @@ const TextEditor: React.FC = () => {
 
   if (editor) {
     return (
-      <div>
-        <MDEditor />
+      <div className="text-editor" ref={ref}>
+        <MDEditor value={value} onChange={(v) => setValue(v || "")} />
       </div>
     );
   }
 
   return (
-    <div onClick={() => setEditor(true)}>
-      <MDEditor.Markdown source={"# Header"} />
+    <div className="text-editor" onClick={() => setEditor(true)}>
+      <div className="card-content">
+        <MDEditor.Markdown source={value} />
+      </div>
     </div>
   );
 };
