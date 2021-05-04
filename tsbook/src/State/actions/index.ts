9b849e7e408150783d84fbcb0ead2a42eb3070c2
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-05-04 23:38:39 +0530

    Redux Setup

--- tsbook/src/State/actions/index.ts
+++ tsbook/src/State/actions/index.ts
@@ -1,7 +1,9 @@
 import { ActionType } from "../action-types";
 import { CellTypes } from "../cell";
 
-interface MoveCellAction {
+export type Direction = "up" | "down";
+
+export interface MoveCellAction {
   type: ActionType.MOVE_CELL;
   payload: {
     id: string;
@@ -9,12 +11,12 @@ interface MoveCellAction {
   };
 }
 
-interface DeleteCellAction {
+export interface DeleteCellAction {
   type: ActionType.DELETE_CELL;
   payload: string;
 }
 
-interface InsertCellBeforeAction {
+export interface InsertCellBeforeAction {
   type: ActionType.INSERT_CELL_BEFORE;
   payload: {
     id: string;
@@ -22,7 +24,7 @@ interface InsertCellBeforeAction {
   };
 }
 
-interface UpdateCellAction {
+export interface UpdateCellAction {
   type: ActionType.UPDATE_CELL;
   payload: {
     id: string;
