788ee4a4616a2b7eb6e517b94a9e039ae405c40a
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-05-06 18:04:52 +0530

    Hooks and CellList component setup

--- tsbook/src/index.tsx
+++ tsbook/src/index.tsx
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom";
 import CodeCell from "./components/code-cell";
 import { Provider } from "react-redux";
 import { store } from "./State";
-import TextEditor from "./components/text-editor";
+import CellList from "./components/cell-list";
 
 const App = () => {
   return (
     <Provider store={store}>
       <div>
-        <TextEditor />
+        <CellList />
       </div>
     </Provider>
   );
