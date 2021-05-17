24c29a1ffb7ed70e7d9080187b689289d4e0ace3
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-05-17 17:39:33 +0530

    Added 'Display' function

--- tsbook/src/components/code-cell.tsx
+++ tsbook/src/components/code-cell.tsx
@@ -18,9 +18,35 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     const { data, order } = state.cells;
     const orderedCells = order.map((id) => data[id]);
 
+    const displayFunc = `
+    import _React from 'react';
+    import _ReactDOM from 'react-dom';
+    
+    var display = (value) => {
+      const root = document.querySelector('#root');
+      if (typeof value === 'object') {
+        if(value.$$typeof && value.props) {
+          _ReactDOM.render(value, root);
+        } else {
+          root.innerHTML = JSON.stringify(value);
+        }
+      } else {
+         root.innerHTML = value;
+      }
+     };
+    `;
+
+    const displayFuncNoop = `var display = () => {
+
+    }`;
     const cummulativeCode = [];
     for (let c of orderedCells) {
       if (c.type === "code") {
+        if (c.id === cell.id) {
+          cummulativeCode.push(displayFunc);
+        } else {
+          cummulativeCode.push(displayFuncNoop);
+        }
         cummulativeCode.push(c.content);
       }
       if (c.id === cell.id) {
@@ -32,13 +58,13 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
 
   useEffect(() => {
     if (!bundle) {
-      createBundle(cell.id, cummulativeCode.join("/n"));
+      createBundle(cell.id, cummulativeCode.join("\n"));
       return;
     }
     // effect
 
     const timer = setTimeout(async () => {
-      createBundle(cell.id, cummulativeCode.join("/n"));
+      createBundle(cell.id, cummulativeCode.join("\n"));
     }, 1000);
     return () => {
       clearTimeout(timer);
