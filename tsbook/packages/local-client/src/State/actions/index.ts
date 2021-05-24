10a13f751c0cd9cde99764111afcb2f426cc8467
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-05-24 23:30:30 +0530

    Adding fetch Cell logic to Redux

--- tsbook/packages/local-client/src/State/actions/index.ts
+++ tsbook/packages/local-client/src/State/actions/index.ts
@@ -1,5 +1,5 @@
 import { ActionType } from "../action-types";
-import { CellTypes } from "../cell";
+import { Cell, CellTypes } from "../cell";
 
 export type Direction = "up" | "down";
 
@@ -50,10 +50,27 @@ export interface BundleCompleteAction {
   };
 }
 
+export interface FetchCellsAction {
+  type: ActionType.FETCH_CELLS;
+}
+
+export interface FetchCellsCompleteAcion {
+  type: ActionType.FETCH_CELLS_COMPLETE;
+  payload: Cell[];
+}
+
+export interface FetchCellsErrorAction {
+  type: ActionType.FETCH_CELLS_ERROR;
+  payload: string;
+}
+
 export type Action =
   | MoveCellAction
   | DeleteCellAction
   | InsertCellAfterAction
   | UpdateCellAction
   | BundleCompleteAction
-  | BundleStartAction;
+  | BundleStartAction
+  | FetchCellsAction
+  | FetchCellsCompleteAcion
+  | FetchCellsErrorAction;
